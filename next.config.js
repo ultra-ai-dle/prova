3d98ca4c203aa0fdd638558c8ada1503b543ade1
seungjoonH <hsj6831@gmail.com>
2026-04-13 18:37:09 +0900

    release: v1.0.0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { version } = require("./package.json");
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_APP_VERSION: version,
+  },
 };
 
 module.exports = nextConfig;
